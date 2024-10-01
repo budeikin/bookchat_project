04bda6bebcee11a946cb2f5782fee7cbb8698886
budeikin <budeikin52@gmail.com>
2024-10-01 11:25:42 +0330

    installing channels and set up settings to running server with ASGI

--- core/core/settings.py
+++ core/core/settings.py
@@ -29,12 +29,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # internal applications
     'accounts.apps.AccountsConfig',
     'home.apps.HomeConfig',
     'book.apps.BookConfig',
@@ -70,6 +73,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
+# daphne
+ASGI_APPLICATION = 'core.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
